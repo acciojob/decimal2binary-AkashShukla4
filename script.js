a0e7996bb9d10e1cc42bde70969c9f5e54b62ade
Akash Shukla <117896950+AkashShukla4@users.noreply.github.com>
2023-05-23 20:15:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function threeSum(arr, target) {
-// write your code here
-  
+
+	let a = new Array(32);
+
+	let i =0;
+	while (target > 0) {
+
+		a[i] = target % 2;
+
+		target = Math.floor(target / 2);
+		i++;
+			}
+   for (let j = i - 1; j >= 0; j--)
+        document.write(a[j]);
+}
 }
 
 module.exports = threeSum;
